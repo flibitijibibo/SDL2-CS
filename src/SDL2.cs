0d070851dd6361049937ea71a33f208c449f7436
David Gow <david@ingeniumdigital.com>
2013-04-05 23:18:47 +0800

    Oops, forgot to add KMODs

--- src/SDL2.cs
+++ src/SDL2.cs
@@ -3072,6 +3072,31 @@ namespace SDL2
 			SDLK_SLEEP = (int)SDL_Scancode.SDL_SCANCODE_SLEEP | SDLK_SCANCODE_MASK
 		}
 
+		/* Key modifiers (bitfield) */
+		[Flags]
+		public enum SDL_Keymod
+		{
+			KMOD_NONE = 0x0000,
+			KMOD_LSHIFT = 0x0001,
+			KMOD_RSHIFT = 0x0002,
+			KMOD_LCTRL = 0x0040,
+			KMOD_RCTRL = 0x0080,
+			KMOD_LALT = 0x0100,
+			KMOD_RALT = 0x0200,
+			KMOD_LGUI = 0x0400,
+			KMOD_RGUI = 0x0800,
+			KMOD_NUM = 0x1000,
+			KMOD_CAPS = 0x2000,
+			KMOD_MODE = 0x4000,
+			KMOD_RESERVED = 0x8000,
+
+			/* These are defines in the SDL headers */
+			KMOD_CTRL = (KMOD_LCTRL | KMOD_RCTRL),
+			KMOD_SHIFT = (KMOD_LSHIFT | KMOD_RSHIFT),
+			KMOD_ALT = (KMOD_LALT | KMOD_RALT),
+			KMOD_GUI = (KMOD_LGUI | KMOD_RGUI)
+		}
+
 		#endregion
 		/* TODO: Force Feedback:
 		 * http://wiki.libsdl.org/moin.fcg/APIByCategory#Force_Feedback
