d1e5b4d1a09b38784596973b63310799b87cf21f
Ethan Lee <flibitijibibo@gmail.com>
2013-10-24 02:28:10 -0400

    2.0.1

--- src/SDL2.cs
+++ src/SDL2.cs
@@ -756,7 +756,7 @@ namespace SDL2
 		 */
 		public const int SDL_MAJOR_VERSION =	2;
 		public const int SDL_MINOR_VERSION =	0;
-		public const int SDL_PATCHLEVEL =	0;
+		public const int SDL_PATCHLEVEL =	1;
 
 		public static readonly int SDL_COMPILEDVERSION = SDL_VERSIONNUM(
 			SDL_MAJOR_VERSION,
