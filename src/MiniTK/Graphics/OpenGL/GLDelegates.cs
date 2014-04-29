7d3217b375f93a194ae2ba3ade1889028f7b64d8
Sean Colombo <sean.colombo@gmail.com>
2014-04-29 10:04:11 -0400

    Adding our custom changes to OpenGL which relate to framebuffers. We probably want to upstream this at some point.

--- src/MiniTK/Graphics/OpenGL/GLDelegates.cs
+++ src/MiniTK/Graphics/OpenGL/GLDelegates.cs
@@ -369,7 +369,7 @@ namespace OpenTK.Graphics.OpenGL
             internal delegate void BlitFramebuffer(Int32 srcX0, Int32 srcY0, Int32 srcX1, Int32 srcY1, Int32 dstX0, Int32 dstY0, Int32 dstX1, Int32 dstY1, OpenTK.Graphics.OpenGL.ClearBufferMask mask, OpenTK.Graphics.OpenGL.BlitFramebufferFilter filter);
             internal static BlitFramebuffer glBlitFramebuffer;
             [System.Security.SuppressUnmanagedCodeSecurity()]
-            internal delegate void BlitFramebufferEXT(Int32 srcX0, Int32 srcY0, Int32 srcX1, Int32 srcY1, Int32 dstX0, Int32 dstY0, Int32 dstX1, Int32 dstY1, OpenTK.Graphics.OpenGL.ClearBufferMask mask, OpenTK.Graphics.OpenGL.ExtFramebufferBlit filter);
+            internal delegate void BlitFramebufferEXT(Int32 srcX0, Int32 srcY0, Int32 srcX1, Int32 srcY1, Int32 dstX0, Int32 dstY0, Int32 dstX1, Int32 dstY1, OpenTK.Graphics.OpenGL.ClearBufferMask mask, OpenTK.Graphics.OpenGL.BlitFramebufferFilter filter);
             internal static BlitFramebufferEXT glBlitFramebufferEXT;
             [System.Security.SuppressUnmanagedCodeSecurity()]
             internal delegate void BufferAddressRangeNV(OpenTK.Graphics.OpenGL.NvVertexBufferUnifiedMemory pname, UInt32 index, UInt64 address, IntPtr length);
