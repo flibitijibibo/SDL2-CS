83012b9866aff26430ba36f1d3a9bc8651274ebe
Ethan Lee <flibitijibibo@gmail.com>
2015-05-21 12:54:41 -0400

    Happy New Year, wait what month is it

--- src/SDL2.cs
+++ src/SDL2.cs
@@ -1,7 +1,7 @@
 #region License
 /* SDL2# - C# Wrapper for SDL2
  *
- * Copyright (c) 2013-2014 Ethan Lee.
+ * Copyright (c) 2013-2015 Ethan Lee.
  *
  * This software is provided 'as-is', without any express or implied warranty.
  * In no event will the authors be held liable for any damages arising from
