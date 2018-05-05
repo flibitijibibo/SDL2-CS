8e0cec49a031204bef8f4aa6b2adc43753aa2043
Ethan Lee <flibitijibibo@gmail.com>
2018-05-05 17:20:00 -0400

    Update PATCHLEVEL

--- src/SDL2.cs
+++ src/SDL2.cs
@@ -866,7 +866,7 @@ namespace SDL2
 		 */
 		public const int SDL_MAJOR_VERSION =	2;
 		public const int SDL_MINOR_VERSION =	0;
-		public const int SDL_PATCHLEVEL =	7;
+		public const int SDL_PATCHLEVEL =	8;
 
 		public static readonly int SDL_COMPILEDVERSION = SDL_VERSIONNUM(
 			SDL_MAJOR_VERSION,
