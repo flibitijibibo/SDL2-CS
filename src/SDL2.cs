9251dbe55f32656efe0d98776c850c5f40a64a02
Caleb Cornett <Caleb.cornett@outlook.com>
2019-03-30 14:04:20 -0400

    Add SDL_UIKitRunApp
    These are the changes needed to use `SDL_UIKitRunApp` with SDL2-CS. This breaks some backwards compatibility since `SDL_WinRT_mainFunction` is now `SDL_main_func` (which is on SDL, not us), and the `argv` parameter is now just a single IntPtr and not an array.
    
    Up to you whether to merge this now or wait till SDL2.0.10 officially launches.

--- src/SDL2.cs
+++ src/SDL2.cs
@@ -168,14 +168,24 @@ namespace SDL2
 		[DllImport(nativeLibName, CallingConvention = CallingConvention.Cdecl)]
 		public static extern void SDL_SetMainReady();
 
-		/* This is used as a function pointer to a C main() function for SDL_WinRTRunApp() */
-		public delegate int SDL_WinRT_mainFunction(int argc, IntPtr[] argv);
+		/* This is used as a function pointer to a C main() function */
+		public delegate int SDL_main_func(int argc, IntPtr argv);
 
 		/* Use this function with UWP to call your C# Main() function! */
 		[DllImport(nativeLibName, CallingConvention = CallingConvention.Cdecl)]
 		public static extern int SDL_WinRTRunApp(
-			SDL_WinRT_mainFunction mainFunction,
+			SDL_main_func mainFunction,
 			IntPtr reserved
+		);
+		
+		/* Use this function with iOS to call your C# Main() function!
+		 * Only available in SDL 2.0.10 or higher.
+		 */
+		[DllImport(nativeLibName, CallingConvention = CallingConvention.Cdecl)]
+		public static extern int SDL_UIKitRunApp(
+			int argc,
+			IntPtr argv,
+			SDL_main_func mainFunction
 		);
 
 		#endregion
